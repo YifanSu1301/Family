db55cef94f97cc4eba2023c807f2e18521b07fa8
LordAndrew312 <andrewsu312@outlook.com>
2022-01-24 23:19:51 -0500

    Initial Commit

---
+++ Family/FamilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyApp.swift
+//  Family
+//
+//  Created by Andrew Su on 2022/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FamilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
